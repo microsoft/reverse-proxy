5f396dd58fbf8a9f093e4ceb5b3e475c2999e242
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-03-31 09:35:12 -0700

    builds and packs

--- src/IslandGateway.Core/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("IslandGateway.Core.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
