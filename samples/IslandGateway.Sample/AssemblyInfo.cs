8273d168b2af24e58381685d920979d904062477
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-03-31 12:12:48 -0700

    Install Arcade (#29)

--- samples/IslandGateway.Sample/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("IslandGateway.Sample.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
