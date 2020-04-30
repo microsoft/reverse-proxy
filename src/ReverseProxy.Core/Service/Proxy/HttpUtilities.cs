4429305e685a4d83f0f8900b14efde92aa45ccf2
Olivier MARTINET <martinet.olivier@gmail.com>
2020-04-30 22:49:14 +0200

    reduce verbosity of HttpUtilities (#119)

--- src/ReverseProxy.Core/Service/Proxy/HttpUtilities.cs
+++ src/ReverseProxy.Core/Service/Proxy/HttpUtilities.cs
@@ -13,55 +13,18 @@ namespace Microsoft.ReverseProxy.Core.Service.Proxy
         /// Converts the given HTTP method (usually obtained from <see cref="HttpRequest.Method"/>)
         /// into the corresponding <see cref="HttpMethod"/> static instance.
         /// </summary>
-        public static HttpMethod GetHttpMethod(string method)
+        public static HttpMethod GetHttpMethod(string method) => method switch
         {
-            if (HttpMethods.IsGet(method))
-            {
-                return HttpMethod.Get;
-            }
-
-            if (HttpMethods.IsPost(method))
-            {
-                return HttpMethod.Post;
-            }
-
-            if (HttpMethods.IsPut(method))
-            {
-                return HttpMethod.Put;
-            }
-
-            if (HttpMethods.IsDelete(method))
-            {
-                return HttpMethod.Delete;
-            }
-
-            if (HttpMethods.IsOptions(method))
-            {
-                return HttpMethod.Options;
-            }
-
-            if (HttpMethods.IsHead(method))
-            {
-                return HttpMethod.Head;
-            }
-
-            if (HttpMethods.IsPatch(method))
-            {
-                return HttpMethod.Patch;
-            }
-
-            if (HttpMethods.IsTrace(method))
-            {
-                return HttpMethod.Trace;
-            }
-
+            string mth when HttpMethods.IsGet(mth) => HttpMethod.Get,
+            string mth when HttpMethods.IsPost(mth) => HttpMethod.Post,
+            string mth when HttpMethods.IsPut(mth) => HttpMethod.Put,
+            string mth when HttpMethods.IsDelete(mth) => HttpMethod.Delete,
+            string mth when HttpMethods.IsOptions(mth) => HttpMethod.Options,
+            string mth when HttpMethods.IsHead(mth) => HttpMethod.Head,
+            string mth when HttpMethods.IsPatch(mth) => HttpMethod.Patch,
+            string mth when HttpMethods.IsTrace(mth) => HttpMethod.Trace,
             // NOTE: Proxying "CONNECT" is not supported (by design!)
-            //if (HttpMethods.IsConnect(method))
-            //{
-            //    return new HttpMethod("CONNECT");
-            //}
-
-            throw new InvalidOperationException($"Unsupported request method '{method}'.");
-        }
+            _ => throw new InvalidOperationException($"Unsupported request method '{method}'.")
+        };
     }
 }
