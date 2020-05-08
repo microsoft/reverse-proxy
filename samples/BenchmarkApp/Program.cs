51f52a61ac082dc313b9b31526f34179a854d6d7
Stephen Halter <halter73@gmail.com>
2020-05-08 12:15:53 -0700

    Add BenchmarkApp

---
+++ samples/BenchmarkApp/Program.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace BenchmarkApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ samples/BenchmarkApp/Startup.cs
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BenchmarkApp
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var urls = _configuration["urls"];
+            var backendUrls = _configuration["backendUrls"];
+
+            if (string.IsNullOrWhiteSpace(urls))
+            {
+                throw new ArgumentException("--urls is required");
+            }
+
+            if (string.IsNullOrWhiteSpace(backendUrls))
+            {
+                throw new ArgumentException("--backendUrls is required");
+            }
+
+            var configDictionary = new Dictionary<string, string>
+            {
+                { "Routes:0:RouteId", "route" },
+                { "Routes:0:BackendId", "backend" },
+                { "Routes:0:Match:Host", new Uri(urls.Split(';', 1)[0]).Host },
+            };
+
+            var backendCount = 0;
+            foreach (var backendUrl in backendUrls.Split(';'))
+            {
+                configDictionary.Add($"Backends:backend:Endpoints:endpoint{backendCount++}:Address", backendUrl);
+            }
+
+            var proxyConfig = new ConfigurationBuilder().AddInMemoryCollection(configDictionary).Build();
+
+            services.AddReverseProxy().LoadFromConfig(proxyConfig);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapReverseProxy();
+            });
+        }
+    }
+}
