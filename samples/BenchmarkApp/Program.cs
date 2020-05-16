2c245cce209f6189e0040acbb4377d4b355f3957
Stephen Halter <halter73@gmail.com>
2020-05-15 19:02:10 -0700

    Output runtime versions during benchmark startup

--- samples/BenchmarkApp/Program.cs
+++ samples/BenchmarkApp/Program.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Reflection;
+using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +13,7 @@ namespace BenchmarkApp
     {
         public static void Main(string[] args)
         {
+            WriteStatistics();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -19,5 +23,25 @@ namespace BenchmarkApp
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void WriteStatistics()
+        {
+            Console.WriteLine("#StartJobStatistics");
+
+            Console.WriteLine(JsonSerializer.Serialize(new {
+                Metadata = new object[]
+                {
+                    new { Source= "Benchmarks", Name= "AspNetCoreVersion", ShortDescription = "ASP.NET Core Version", LongDescription = "ASP.NET Core Version" },
+                    new { Source= "Benchmarks", Name= "NetCoreAppVersion", ShortDescription = ".NET Runtime Version", LongDescription = ".NET Runtime Version" },
+                },
+                Measurements = new object[]
+                {
+                    new { Timestamp = DateTime.UtcNow, Name = "AspNetCoreVersion", Value = typeof(WebHostBuilder).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
+                    new { Timestamp = DateTime.UtcNow, Name = "NetCoreAppVersion", Value = typeof(object).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
+                }
+            }));
+
+            Console.WriteLine("#EndJobStatistics");
+        }
     }
 }
