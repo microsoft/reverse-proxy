a7d9cc833d97e4ebe12f535012ae7b5a3e37db78
Stephen Halter <halter73@gmail.com>
2020-05-18 15:34:06 -0700

    PR feedback

--- samples/BenchmarkApp/Program.cs
+++ samples/BenchmarkApp/Program.cs
@@ -24,24 +24,24 @@ namespace BenchmarkApp
                     webBuilder.UseStartup<Startup>();
                 });
 
-        private static void WriteStatistics()
-        {
-            Console.WriteLine("#StartJobStatistics");
-
-            Console.WriteLine(JsonSerializer.Serialize(new {
-                Metadata = new object[]
+        private static void WriteStatistics() =>
+            Console.WriteLine(
+                "#StartJobStatistics"
+                + Environment.NewLine
+                + JsonSerializer.Serialize(new
                 {
-                    new { Source= "Benchmarks", Name= "AspNetCoreVersion", ShortDescription = "ASP.NET Core Version", LongDescription = "ASP.NET Core Version" },
-                    new { Source= "Benchmarks", Name= "NetCoreAppVersion", ShortDescription = ".NET Runtime Version", LongDescription = ".NET Runtime Version" },
-                },
-                Measurements = new object[]
-                {
-                    new { Timestamp = DateTime.UtcNow, Name = "AspNetCoreVersion", Value = typeof(WebHostBuilder).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
-                    new { Timestamp = DateTime.UtcNow, Name = "NetCoreAppVersion", Value = typeof(object).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
-                }
-            }));
-
-            Console.WriteLine("#EndJobStatistics");
-        }
+                    Metadata = new object[]
+                    {
+                        new { Source= "Benchmarks", Name= "AspNetCoreVersion", ShortDescription = "ASP.NET Core Version", LongDescription = "ASP.NET Core Version" },
+                        new { Source= "Benchmarks", Name= "NetCoreAppVersion", ShortDescription = ".NET Runtime Version", LongDescription = ".NET Runtime Version" },
+                    },
+                    Measurements = new object[]
+                    {
+                        new { Timestamp = DateTime.UtcNow, Name = "AspNetCoreVersion", Value = typeof(IWebHostBuilder).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
+                        new { Timestamp = DateTime.UtcNow, Name = "NetCoreAppVersion", Value = typeof(object).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion },
+                    }
+                })
+                + Environment.NewLine
+                + "#EndJobStatistics");
     }
 }
