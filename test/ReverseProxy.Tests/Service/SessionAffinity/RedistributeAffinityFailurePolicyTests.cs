d63c9f448939a20959b3b0622f06f699d8f5e4c4
Alexander Nikolaev <alnikola@microsoft.com>
2020-06-08 16:53:34 +0200

    Fix RedistributeAffinityFailurePolicyTests

--- test/ReverseProxy.Tests/Service/SessionAffinity/RedistributeAffinityFailurePolicyTests.cs
+++ test/ReverseProxy.Tests/Service/SessionAffinity/RedistributeAffinityFailurePolicyTests.cs
@@ -13,8 +13,9 @@ namespace Microsoft.ReverseProxy.Service.SessionAffinity
     public class RedistributeAffinityFailurePolicyTests
     {
         [Theory]
-        [MemberData(nameof(HandleCases))]
-        public async Task Handle_AnyAffinitStatus_ReturnTrue(AffinityStatus status)
+        [InlineData(AffinityStatus.AffinityKeyExtractionFailed)]
+        [InlineData(AffinityStatus.DestinationNotFound)]
+        public async Task Handle_FailedAffinityStatus_ReturnTrue(AffinityStatus status)
         {
             var policy = new RedistributeAffinityFailurePolicy();
 
@@ -31,16 +32,6 @@ namespace Microsoft.ReverseProxy.Service.SessionAffinity
             var context = new DefaultHttpContext();
 
             await Assert.ThrowsAsync<InvalidOperationException>(() => policy.Handle(context, default, status));
-        }
-
-        public static IEnumerable<object[]> HandleCases()
-        {
-            // Successful statuses are also included because redistribute policy is not supposed to validate this parameter
-            // and therefore must react properly to all affinity statuses.
-            foreach(AffinityStatus status in Enum.GetValues(typeof(AffinityStatus)))
-            {
-                yield return new object[] { status };
-            }
         }
     }
 }
