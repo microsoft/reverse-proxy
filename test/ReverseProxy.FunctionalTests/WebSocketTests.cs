f3ec19c261749c06c288dca254c5e5315ec4f5dd
Kahbazi <A.Kahbazi@gmail.com>
2020-07-14 20:48:42 +0430

    Add test for raw upgrade

--- test/ReverseProxy.FunctionalTests/WebSocketTests.cs
+++ test/ReverseProxy.FunctionalTests/WebSocketTests.cs
@@ -1,21 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
-using Xunit;
-using System.Net.WebSockets;
-using System.Threading;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.ReverseProxy.Configuration;
 using Microsoft.ReverseProxy.Abstractions;
+using Microsoft.ReverseProxy.Configuration;
+using Xunit;
 
 namespace Microsoft.ReverseProxy
 {
@@ -24,7 +23,7 @@ namespace Microsoft.ReverseProxy
         [Theory]
         [InlineData(WebSocketMessageType.Binary)]
         [InlineData(WebSocketMessageType.Text)]
-        public async Task WebSocketEchoTest(WebSocketMessageType messageType)
+        public async Task WebSocketTest(WebSocketMessageType messageType)
         {
             var destinationHost = CreateWebSocketHost();
             await destinationHost.StartAsync();
@@ -36,7 +35,7 @@ namespace Microsoft.ReverseProxy
 
             var client = new ClientWebSocket();
             var webSocketsTarget = proxyHostUrl.Replace("https://", "wss://").Replace("http://", "ws://");
-            var targetUri = new Uri(new Uri(webSocketsTarget, UriKind.Absolute), "/");
+            var targetUri = new Uri(new Uri(webSocketsTarget, UriKind.Absolute), "/websockets");
             await client.ConnectAsync(targetUri, CancellationToken.None);
 
             var buffer = new byte[1024];
@@ -49,8 +48,60 @@ namespace Microsoft.ReverseProxy
 
             var message = await client.ReceiveAsync(buffer, CancellationToken.None);
 
+            Assert.Equal(messageType, message.MessageType);
+            Assert.True(message.EndOfMessage);
+
             var text = Encoding.UTF8.GetString(buffer.AsSpan(0, message.Count));
             Assert.Equal(textToSend, text);
+        }
+
+        [Fact]
+        public async Task RawUpgradeTest()
+        {
+            var destinationHost = CreateWebSocketHost();
+            await destinationHost.StartAsync();
+            var destinationHostUrl = Helpers.GetAddress(destinationHost);
+
+            var proxyHost = CreateReverseProxyHost(destinationHostUrl);
+            await proxyHost.StartAsync();
+            var proxyHostUrl = Helpers.GetAddress(proxyHost);
+
+            using var handler = new HttpClientHandler
+            {
+                AllowAutoRedirect = false,
+                AutomaticDecompression = DecompressionMethods.None,
+                UseCookies = false,
+                UseProxy = false
+            };
+            using var client = new HttpMessageInvoker(handler);
+            var webSocketsTarget = proxyHostUrl.Replace("https://", "wss://").Replace("http://", "ws://");
+            var targetUri = new Uri(new Uri(webSocketsTarget, UriKind.Absolute), "rawupgrade");
+            var request = new HttpRequestMessage(HttpMethod.Get, targetUri);
+            request.Headers.TryAddWithoutValidation("Connection", "upgrade");
+            request.Version = new Version(1, 1);
+
+            var response = await client.SendAsync(request, CancellationToken.None);
+
+            Assert.Equal(HttpStatusCode.SwitchingProtocols, response.StatusCode);
+
+            var rawStream = await response.Content.ReadAsStreamAsync();
+
+            var buffer = new byte[1];
+            for (var i = 0; i <= 255; i++)
+            {
+                buffer[0] = (byte)i;
+                await rawStream.WriteAsync(buffer, 0, 1, CancellationToken.None);
+                var read = await rawStream.ReadAsync(buffer, CancellationToken.None);
+                if (i == 255)
+                {
+                    Assert.Equal(1, read);
+                }
+                else
+                {
+                    Assert.Equal(1, read);
+                    Assert.Equal(i, buffer[0]);
+                }
+            }
         }
 
         private IHost CreateReverseProxyHost(string destinationHostUrl)
@@ -67,7 +118,7 @@ namespace Microsoft.ReverseProxy
                         RouteId = "route1",
                         ClusterId = "cluster1"
                     };
-                    proxyRoute.Match.Path = "/";
+                    proxyRoute.Match.Path = "/{**catchall}";
 
                     x.Routes.Add(proxyRoute);
 
@@ -95,7 +146,7 @@ namespace Microsoft.ReverseProxy
                 {
                     var endpoint = context.GetEndpoint();
 
-                     return next();
+                    return next();
                 });
                 app.UseEndpoints(builder =>
                 {
@@ -115,11 +166,12 @@ namespace Microsoft.ReverseProxy
                 app.UseRouting();
                 app.UseEndpoints(builder =>
                 {
-                    builder.Map("/", RunPingPongAsync);
+                    builder.Map("/websockets", WebSocket);
+                    builder.Map("/rawupgrade", RawUpgrade);
                 });
             });
 
-            static async Task RunPingPongAsync(HttpContext httpcontext)
+            static async Task WebSocket(HttpContext httpcontext)
             {
                 if (!httpcontext.WebSockets.IsWebSocketRequest)
                 {
@@ -145,9 +197,33 @@ namespace Microsoft.ReverseProxy
                     }
                 }
             }
+
+            static async Task RawUpgrade(HttpContext httpContext)
+            {
+                var upgradeFeature = httpContext.Features.Get<IHttpUpgradeFeature>();
+                if (upgradeFeature == null || !upgradeFeature.IsUpgradableRequest)
+                {
+                    httpContext.Response.StatusCode = StatusCodes.Status426UpgradeRequired;
+                    return;
+                }
+
+                await using var stream = await upgradeFeature.UpgradeAsync();
+                var buffer = new byte[1];
+                int read;
+                while ((read = await stream.ReadAsync(buffer, httpContext.RequestAborted)) != 0)
+                {
+                    await stream.WriteAsync(buffer, 0, read);
+
+                    if (buffer[0] == 255)
+                    {
+                        // Goodbye
+                        break;
+                    }
+                }
+            }
         }
 
-        public IHost CreateHost(Action<IServiceCollection> configureServices, Action<IApplicationBuilder> configureApp)
+        private IHost CreateHost(Action<IServiceCollection> configureServices, Action<IApplicationBuilder> configureApp)
         {
             return new HostBuilder()
                .ConfigureWebHost(webHostBuilder =>
