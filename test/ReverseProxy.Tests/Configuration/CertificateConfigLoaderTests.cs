fa97db0006255caee4c4763f1db182e87616aad3
MihaZupan <mihazupan.zupan1@gmail.com>
2020-09-15 18:42:54 +0200

    Use inline suppressions

--- test/ReverseProxy.Tests/Configuration/CertificateConfigLoaderTests.cs
+++ test/ReverseProxy.Tests/Configuration/CertificateConfigLoaderTests.cs
@@ -94,6 +94,7 @@ namespace Microsoft.ReverseProxy.Configuration
             {
                 Path = TestResources.GetCertPath("https-aspnet.crt"),
                 KeyPath = TestResources.GetCertPath("https-aspnet.key"),
+                // [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Unit test dummy credentials.")]
                 Password = "abcde"
             };
             Assert.ThrowsAny<CryptographicException>(() => loader.LoadCertificate(options));
