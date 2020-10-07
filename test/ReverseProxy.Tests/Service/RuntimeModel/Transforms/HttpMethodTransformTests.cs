53cb8cdcbecd9120669a96deee486c34480c146e
Chris Ross <Tratcher@Outlook.com>
2020-10-07 14:30:35 -0700

    Update test/ReverseProxy.Tests/Service/RuntimeModel/Transforms/HttpMethodTransformTests.cs

--- test/ReverseProxy.Tests/Service/RuntimeModel/Transforms/HttpMethodTransformTests.cs
+++ test/ReverseProxy.Tests/Service/RuntimeModel/Transforms/HttpMethodTransformTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Microsoft.ReverseProxy.Service.RuntimeModel.Transforms
