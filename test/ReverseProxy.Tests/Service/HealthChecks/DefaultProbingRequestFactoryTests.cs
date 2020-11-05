0b2fe2dcbc13038db7b8804e1f6d9ed8ae50ea34
Marie Píchová <mapichov@microsoft.com>
2020-11-05 19:52:38 +0100

    Specific probing tests for request version.

--- test/ReverseProxy.Tests/Service/HealthChecks/DefaultProbingRequestFactoryTests.cs
+++ test/ReverseProxy.Tests/Service/HealthChecks/DefaultProbingRequestFactoryTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Microsoft.ReverseProxy.Abstractions;
@@ -21,18 +23,56 @@ namespace Microsoft.ReverseProxy.Service.HealthChecks
         [InlineData("https://localhost:10000/", "https://localhost:20000/api", "/health/", "https://localhost:20000/api/health/")]
         public void CreateRequest_HealthEndpointIsNotDefined_UseDestinationAddress(string address, string health, string healthPath, string expectedRequestUri)
         {
-            var version = HttpVersion.Version11;
-            var clusterConfig = GetClusterConfig("cluster0", new ClusterActiveHealthCheckOptions(true, null, null, "policy", healthPath), version);
+            var clusterConfig = GetClusterConfig("cluster0", new ClusterActiveHealthCheckOptions(true, null, null, "policy", healthPath), HttpVersion.Version20);
             var destinationConfig = new DestinationConfig(address, health);
             var factory = new DefaultProbingRequestFactory();
 
             var request = factory.CreateRequest(clusterConfig, destinationConfig);
 
             Assert.Equal(expectedRequestUri, request.RequestUri.AbsoluteUri);
-            Assert.Equal(version, request.Version);
         }
 
-        private ClusterConfig GetClusterConfig(string id, ClusterActiveHealthCheckOptions healthCheckOptions, Version version)
+        [Theory]
+        [MemberData(nameof(RequestVersionMemberData))]
+        public void CreateRequest_RequestVersionProperties(Version version
+#if NET
+            , HttpVersionPolicy versionPolicy
+#endif
+        )
+        {
+            var clusterConfig = GetClusterConfig("cluster0", new ClusterActiveHealthCheckOptions(true, null, null, "policy", null), version
+#if NET
+                , versionPolicy
+#endif
+                );
+            var destinationConfig = new DestinationConfig("https://localhost:10000/", null);
+            var factory = new DefaultProbingRequestFactory();
+
+            var request = factory.CreateRequest(clusterConfig, destinationConfig);
+
+            Assert.Equal(version ?? HttpVersion.Version20, request.Version);
+#if NET
+            Assert.Equal(versionPolicy, request.VersionPolicy);
+#endif
+        }
+
+        public static IEnumerable<object[]> RequestVersionMemberData() =>
+            from version in new[] { HttpVersion.Version10, HttpVersion.Version11, HttpVersion.Version20, null }
+#if NET
+            from policy in new [] { HttpVersionPolicy.RequestVersionExact, HttpVersionPolicy.RequestVersionOrHigher, HttpVersionPolicy.RequestVersionOrLower }
+#endif
+            select new object[] {
+                version
+#if NET
+                , policy
+#endif
+            };
+
+        private ClusterConfig GetClusterConfig(string id, ClusterActiveHealthCheckOptions healthCheckOptions, Version version
+#if NET
+            , HttpVersionPolicy versionPolicy = HttpVersionPolicy.RequestVersionExact
+#endif
+            )
         {
             return new ClusterConfig(
                 new Cluster { Id = id },
@@ -45,7 +85,7 @@ namespace Microsoft.ReverseProxy.Service.HealthChecks
                     TimeSpan.FromSeconds(60),
                     version
 #if NET
-                    , HttpVersionPolicy.RequestVersionExact
+                    , versionPolicy
 #endif
                     ), null);
         }
