2f15456b0ead8c66223871390204ad4be1a49fbf
MihaZupan <mihazupan.zupan1@gmail.com>
2020-12-08 08:35:39 +0100

    Endpoint => Destination

--- src/ReverseProxy/Abstractions/ClusterDiscovery/Contract/LoadBalancingPolicies.cs
+++ src/ReverseProxy/Abstractions/ClusterDiscovery/Contract/LoadBalancingPolicies.cs
@@ -9,28 +9,28 @@ namespace Microsoft.ReverseProxy.Abstractions.ClusterDiscovery.Contract
     public static class LoadBalancingPolicies
     {
         /// <summary>
-        /// Select the first endpoint without considering load. This is useful for dual endpoint fail-over systems.
+        /// Select the first destination without considering load. This is useful for dual destination fail-over systems.
         /// </summary>
         public static string First => nameof(First);
 
         /// <summary>
-        /// Select an endpoint randomly.
+        /// Select a destination randomly.
         /// </summary>
         public static string Random => nameof(Random);
 
         /// <summary>
-        /// Selects an endpoint by cycling through them in order.
+        /// Select a destination by cycling through them in order.
         /// </summary>
         public static string RoundRobin => nameof(RoundRobin);
 
         /// <summary>
-        /// Select the endpoint with the least assigned requests. This requires examining all nodes.
+        /// Select the destination with the least assigned requests. This requires examining all nodes.
         /// </summary>
         public static string LeastRequests => nameof(LeastRequests);
 
         /// <summary>
-        /// Select two random endpoints and then select the one with the least assigned requests.
-        /// This avoids the overhead of LeastRequests and the worst case for Random where it selects a busy endpoint.
+        /// Select two random destinations and then select the one with the least assigned requests.
+        /// This avoids the overhead of LeastRequests and the worst case for Random where it selects a busy destination.
         /// </summary>
         public static string PowerOfTwoChoices => nameof(PowerOfTwoChoices);
     }
