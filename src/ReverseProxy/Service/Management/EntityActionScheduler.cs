1e620ac259eac6fa1f16880002a4215d240aa905
Kahbazi <A.Kahbazi@gmail.com>
2021-03-23 16:35:09 +0430

    Dispose the timer (#865)
    * Dispose the timer
    
    * Assert an entry is always added

--- src/ReverseProxy/Service/Management/EntityActionScheduler.cs
+++ src/ReverseProxy/Service/Management/EntityActionScheduler.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Yarp.ReverseProxy.Utilities;
@@ -62,7 +63,8 @@ namespace Yarp.ReverseProxy.Service.Management
         public void ScheduleEntity(T entity, TimeSpan period)
         {
             var entry = new SchedulerEntry(entity, (long)period.TotalMilliseconds, _timerCallback, Volatile.Read(ref _isStarted) == 1, _timerFactory);
-            _entries.TryAdd(entity, entry);
+            var added = _entries.TryAdd(entity, entry);
+            Debug.Assert(added);
         }
 
         public void ChangePeriod(T entity, TimeSpan newPeriod)
