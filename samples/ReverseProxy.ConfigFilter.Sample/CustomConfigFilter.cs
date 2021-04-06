ab57cd423f01df18894bf2dc38ccfb0f69a847f2
Sam Spencer <54915162+samsp-msft@users.noreply.github.com>
2021-04-06 11:00:12 -0700

    added missing using

--- samples/ReverseProxy.ConfigFilter.Sample/CustomConfigFilter.cs
+++ samples/ReverseProxy.ConfigFilter.Sample/CustomConfigFilter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading;
