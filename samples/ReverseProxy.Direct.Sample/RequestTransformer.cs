a866d7910662125e3165836d4899a562b1693660
Kahbazi <A.Kahbazi@gmail.com>
2021-08-05 01:04:25 +0430

    Add extensions method for HttpForwarder in sample

---
+++ samples/ReverseProxy.Direct.Sample/RequestTransformer.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Yarp.ReverseProxy.Forwarder;
+
+namespace Yarp.Sample
+{
+    internal class RequestTransformer : HttpTransformer
+    {
+        private readonly Func<HttpContext, HttpRequestMessage, ValueTask> _requestTransform;
+
+        public RequestTransformer(Func<HttpContext, HttpRequestMessage, ValueTask> requestTransform)
+        {
+            _requestTransform = requestTransform;
+        }
+
+        public override async ValueTask TransformRequestAsync(HttpContext httpContext, HttpRequestMessage proxyRequest, string destinationPrefix)
+        {
+            await base.TransformRequestAsync(httpContext, proxyRequest, destinationPrefix);
+            await _requestTransform(httpContext, proxyRequest);
+        }
+    }
+}
