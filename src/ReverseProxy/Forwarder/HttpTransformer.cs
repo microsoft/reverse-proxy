5641f49eb45f5b2789a162dc128fd41648b224d3
Alexander Nikolaev <55398552+alnikola@users.noreply.github.com>
2021-09-16 09:58:01 +0200

    Use the same default transformer for HttpForwarder and the whole YARP (#1246)
    Fixes #1093

--- src/ReverseProxy/Forwarder/HttpTransformer.cs
+++ src/ReverseProxy/Forwarder/HttpTransformer.cs
@@ -11,16 +11,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
+using Yarp.ReverseProxy.Transforms.Builder;
 
 namespace Yarp.ReverseProxy.Forwarder
 {
     public class HttpTransformer
     {
         /// <summary>
-        /// A default set of transforms that copies all request and response fields and headers, except for some
-        /// protocol specific values.
+        /// A default set of transforms that adds X-Forwarded-* headers, removes the original Host value and
+        /// copies all other request and response fields and headers, except for some protocol specific values.
         /// </summary>
-        public static readonly HttpTransformer Default = new HttpTransformer();
+        public static readonly HttpTransformer Default;
+
+        static HttpTransformer()
+        {
+            Default = TransformBuilder.CreateTransformer(new TransformBuilderContext());
+        }
 
         /// <summary>
         /// Used to create derived instances.
