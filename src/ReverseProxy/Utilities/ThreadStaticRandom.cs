56e3e351299cf0a1cda46b9a6ed20d342da81e25
Miha Zupan <mihazupan.zupan1@gmail.com>
2021-10-19 12:58:52 +0200

    Use Random.Shared

--- src/ReverseProxy/Utilities/ThreadStaticRandom.cs
+++ src/ReverseProxy/Utilities/ThreadStaticRandom.cs
@@ -10,8 +10,12 @@ namespace Yarp.ReverseProxy.Utilities
     /// </summary>
     internal sealed class ThreadStaticRandom
     {
+#if NET6_0_OR_GREATER
+        public static Random Instance => Random.Shared;
+#else
         [ThreadStatic]
         private static Random? t_inst;
         public static Random Instance => t_inst ??= new Random();
+#endif
     }
 }
