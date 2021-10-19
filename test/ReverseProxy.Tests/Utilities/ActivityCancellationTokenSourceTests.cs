2a506228fc17630a7f55a7b8f62ffe2220f87dd2
Miha Zupan <mihazupan.zupan1@gmail.com>
2021-10-19 14:15:20 +0200

    Don't assert the sources are pooled before 6.0

--- test/ReverseProxy.Tests/Utilities/ActivityCancellationTokenSourceTests.cs
+++ test/ReverseProxy.Tests/Utilities/ActivityCancellationTokenSourceTests.cs
@@ -10,16 +10,40 @@ namespace Yarp.ReverseProxy.Utilities.Tests
 {
     public class ActivityCancellationTokenSourceTests
     {
+#if NET6_0_OR_GREATER // CancellationTokenSource.TryReset() is only available in 6.0+
         [Fact]
         public void ActivityCancellationTokenSource_PoolsSources()
         {
-            var cts = ActivityCancellationTokenSource.Rent(TimeSpan.FromSeconds(10), CancellationToken.None);
+            // This test can run in parallel with others making use of ActivityCancellationTokenSource
+            // A different thread could have already added/removed a source from the queue
 
-            cts.Return();
+            for (var i = 0; i < 1000; i++)
+            {
+                var cts = ActivityCancellationTokenSource.Rent(TimeSpan.FromSeconds(10), CancellationToken.None);
+                cts.Return();
+
+                var cts2 = ActivityCancellationTokenSource.Rent(TimeSpan.FromSeconds(10), CancellationToken.None);
+                cts2.Return();
+
+                if (ReferenceEquals(cts, cts2))
+                {
+                    return;
+                }
+            }
+
+            Assert.True(false, "CancellationTokenSources were not pooled");
+        }
+#endif
+
+        [Fact]
+        public void ActivityCancellationTokenSource_DoesNotPoolsCanceledSources()
+        {
+            var cts = ActivityCancellationTokenSource.Rent(TimeSpan.FromSeconds(10), CancellationToken.None);
+            cts.Cancel();
 
             var cts2 = ActivityCancellationTokenSource.Rent(TimeSpan.FromSeconds(10), CancellationToken.None);
 
-            Assert.Same(cts, cts2);
+            Assert.NotSame(cts, cts2);
         }
 
         [Fact]
@@ -61,7 +85,7 @@ namespace Yarp.ReverseProxy.Utilities.Tests
                 await Task.Delay(1);
             }
 
-            throw new TimeoutException("Cts was not canceled");
+            Assert.True(false, "Cts was not canceled");
         }
     }
 }
