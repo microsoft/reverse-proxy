e24713f88fe5532eceb64192883c65ca1acd1af7
Andrew Stakhov <andrew.stakhov@gmail.com>
2022-02-02 15:42:05 -0500

    Add support for extended YARP config in Kubernetes via annotations (#1263)
    * Add support for extended YARP config in Kubernetes via annotations
    
    * Add comment on the use of json serializer
    
    * Switch block annotation format from JSON to YAML
    
    * Formatting fixes
    
    * Add default namespaces

--- src/Kubernetes.Controller/Converters/YarpIngressOptions.cs
+++ src/Kubernetes.Controller/Converters/YarpIngressOptions.cs
@@ -1,9 +1,19 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using Yarp.ReverseProxy.Configuration;
+
 namespace Yarp.Kubernetes.Controller.Services;
 
 internal sealed class YarpIngressOptions
 {
     public bool Https { get; set; }
+    public List<Dictionary<string,string>> Transforms { get; set; }
+    public string AuthorizationPolicy { get; set; }
+    public SessionAffinityConfig SessionAffinity { get; set; }
+    public HttpClientConfig HttpClientConfig { get; set; }
+    public string LoadBalancingPolicy { get; set; }
+    public string CorsPolicy { get; set; }
+    public HealthCheckConfig HealthCheck { get; set; }
 }
