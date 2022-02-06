61278084a26aa7ff250952e5b699b91a04d2bf19
jerry-shao <com.jerryshao@jerryshao.com>
2022-02-06 23:02:38 +0000

    Changing QueryTransformContext query collection to be case insensitive

---
+++ test/ReverseProxy.Tests/Transforms/QueryTransformContextTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+namespace Yarp.ReverseProxy.Transforms.Tests;
+
+public class QueryTransformContextTests
+{
+    [Fact]
+    public void Collection_TryGetValue_CaseInsensitive()
+    {
+        var httpContext = new DefaultHttpContext {Request = {QueryString = new QueryString("?z=1")}};
+        var queryTransformContext = new QueryTransformContext(httpContext.Request);
+        queryTransformContext.Collection.TryGetValue("Z", out var result);
+        Assert.Equal("1", result);
+    }
+
+    [Fact]
+    public void Collection_RemoveKey_CaseInsensitive()
+    {
+        var httpContext = new DefaultHttpContext { Request = { QueryString = new QueryString("?z=1") } };
+        var queryTransformContext = new QueryTransformContext(httpContext.Request);
+        queryTransformContext.Collection.Remove("Z");
+        Assert.False(queryTransformContext.QueryString.HasValue);
+    }
+}
