09cfbfd20629dbb3fd631bf2df5ff42719b7fee3
Chris R <Tratcher@outlook.com>
2022-02-09 15:06:09 -0800

    API docs

--- src/ReverseProxy/Management/IProxyStateLookup.cs
+++ src/ReverseProxy/Management/IProxyStateLookup.cs
@@ -7,13 +7,28 @@ using Yarp.ReverseProxy.Model;
 
 namespace Yarp.ReverseProxy;
 
+/// <summary>
+/// Allows access to the proxy's current set of routes and clusters.
+/// </summary>
 public interface IProxyStateLookup
 {
+    /// <summary>
+    /// Retrieves a specific route by id, if present.
+    /// </summary>
     bool TryGetRoute(string id, [NotNullWhen(true)] out RouteModel? route);
 
+    /// <summary>
+    /// Enumerates all current routes. This is thread safe but the collection may change mid enumeration if the configuration is reloaded.
+    /// </summary>
     IEnumerable<RouteModel> GetRoutes();
 
+    /// <summary>
+    /// Retrieves a specific cluster by id, if present.
+    /// </summary>
     bool TryGetCluster(string id, [NotNullWhen(true)] out ClusterState? cluster);
 
+    /// <summary>
+    /// Enumerates all current clusters. This is thread safe but the collection may change mid enumeration if the configuration is reloaded.
+    /// </summary>
     IEnumerable<ClusterState> GetClusters();
 }
