548bb1665f30acd7e1019616986cc4f78a23a53d
witskeeper <witskeeper@gmail.com>
2022-11-01 20:58:09 +0800

    patchtype

--- src/Kubernetes.Controller/Client/V1IngressResourceStatusUpdater.cs
+++ src/Kubernetes.Controller/Client/V1IngressResourceStatusUpdater.cs
@@ -38,14 +38,14 @@ internal class V1IngressResourceStatusUpdater : IIngressResourceStatusUpdater
             var loadBalancerIngresses = service.Status.LoadBalancer.Ingress;
             V1IngressStatus status = new V1IngressStatus(new V1LoadBalancerStatus(loadBalancerIngresses));
             //V1Patch patch = new V1Patch(status, V1Patch.PatchType.ApplyPatch);
-            
+
             var ingresses = _cache.GetIngresses().ToArray();
             foreach (var ingress in ingresses)
             {
                 _logger.LogInformation("updating ingress {IngressClassNamespace}/{IngressClassName} status.", ingress.Metadata.NamespaceProperty, ingress.Metadata.Name);
                 var s = await _client.NetworkingV1.ReadNamespacedIngressStatusAsync(ingress.Metadata.Name, ingress.Metadata.NamespaceProperty);
                 s.Status = status;
-                await _client.NetworkingV1.PatchNamespacedIngressStatusAsync(new V1Patch(s), ingress.Metadata.Name, ingress.Metadata.NamespaceProperty);
+                await _client.NetworkingV1.PatchNamespacedIngressStatusAsync(new V1Patch(s, V1Patch.PatchType.ApplyPatch), ingress.Metadata.Name, ingress.Metadata.NamespaceProperty);
                 _logger.LogInformation("updated ingrees {IngressClassNamespace}/{IngressClassName} status.", ingress.Metadata.NamespaceProperty, ingress.Metadata.Name);
             }
         }
