6884ef1a9c8f99237f21a47c1e622ca2cc8cfd54
Chris R <Tratcher@outlook.com>
2022-12-19 11:36:04 -0800

    Use C# 11 syntax

--- src/ReverseProxy/Forwarder/ProtocolHelper.cs
+++ src/ReverseProxy/Forwarder/ProtocolHelper.cs
@@ -43,16 +43,6 @@ internal static class ProtocolHelper
         && MediaTypeHeaderValue.TryParse(contentType, out var mediaType)
         && mediaType.MatchesMediaType(GrpcContentType);
 
-    // GUID appended by the server as part of the security key response.  Defined in the RFC.
-    private static readonly byte[] _wsServerGuidBytes = new byte[]
-    {
-            (byte)'2', (byte)'5', (byte)'8', (byte)'E', (byte)'A', (byte)'F', (byte)'A', (byte)'5', (byte)'-',
-            (byte)'E', (byte)'9', (byte)'1', (byte)'4', (byte)'-',
-            (byte)'4', (byte)'7', (byte)'D', (byte)'A', (byte)'-',
-            (byte)'9', (byte)'5', (byte)'C', (byte)'A', (byte)'-',
-            (byte)'C', (byte)'5', (byte)'A', (byte)'B', (byte)'0', (byte)'D', (byte)'C', (byte)'8', (byte)'5', (byte)'B', (byte)'1', (byte)'1'
-    };
-
     /// <summary>
     /// Creates a security key for sending in the Sec-WebSocket-Key header.
     /// </summary>
@@ -60,7 +50,6 @@ internal static class ProtocolHelper
     internal static string CreateSecWebSocketKey()
     {
         Span<byte> bytes = stackalloc byte[16];
-
         // Base64-encode a new Guid's bytes to get the security key
         var success = Guid.NewGuid().TryWriteBytes(bytes);
         Debug.Assert(success);
@@ -75,11 +64,13 @@ internal static class ProtocolHelper
     /// <returns>A key-value pair of the request header security key and expected response header value.</returns>
     internal static string CreateSecWebSocketAccept(string key)
     {
-        Span<byte> bytes = stackalloc byte[24 /* Base64 guid length */ + _wsServerGuidBytes.Length];
+        // GUID appended by the server as part of the security key response.  Defined in the RFC.
+        var wsServerGuidBytes = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"u8;
+        Span<byte> bytes = stackalloc byte[24 /* Base64 guid length */ + wsServerGuidBytes.Length];
 
         // Get the corresponding ASCII bytes for seckey+wsServerGuidBytes
         var encodedSecKeyLength = Encoding.ASCII.GetBytes(key, bytes);
-        _wsServerGuidBytes.CopyTo(bytes.Slice(encodedSecKeyLength));
+        wsServerGuidBytes.CopyTo(bytes.Slice(encodedSecKeyLength));
 
         // Hash the seckey+wsServerGuidBytes bytes
         SHA1.TryHashData(bytes, bytes, out var bytesWritten);
