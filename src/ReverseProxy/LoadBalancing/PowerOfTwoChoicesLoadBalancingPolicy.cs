6b987b185b532e118f7ee3c17211081e425ed598
Sam Spencer <samsp@microsoft.com>
2023-07-07 08:54:37 -0700

    power of 2

--- src/ReverseProxy/LoadBalancing/PowerOfTwoChoicesLoadBalancingPolicy.cs
+++ src/ReverseProxy/LoadBalancing/PowerOfTwoChoicesLoadBalancingPolicy.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Yarp.ReverseProxy.Model;
 using Yarp.ReverseProxy.Utilities;
+using Yarp.ReverseProxy.Weighting;
 
 namespace Yarp.ReverseProxy.LoadBalancing;
 
 internal sealed class PowerOfTwoChoicesLoadBalancingPolicy : ILoadBalancingPolicy
 {
     private readonly IRandomFactory _randomFactory;
+    private readonly IProxyWeightingProvider _weightingProvider;
 
-    public PowerOfTwoChoicesLoadBalancingPolicy(IRandomFactory randomFactory)
+    public PowerOfTwoChoicesLoadBalancingPolicy(IRandomFactory randomFactory, IProxyWeightingProvider weightingProvider)
     {
         _randomFactory = randomFactory;
+        _weightingProvider = weightingProvider;
     }
 
     public string Name => LoadBalancingPolicies.PowerOfTwoChoices;
@@ -26,7 +29,7 @@ internal sealed class PowerOfTwoChoicesLoadBalancingPolicy : ILoadBalancingPolic
         {
             return null;
         }
-        
+
         if (destinationCount == 1)
         {
             return availableDestinations[0];
@@ -34,15 +37,23 @@ internal sealed class PowerOfTwoChoicesLoadBalancingPolicy : ILoadBalancingPolic
 
         // Pick two, and then return the least busy. This avoids the effort of searching the whole list, but
         // still avoids overloading a single destination.
-        var random = _randomFactory.CreateRandomInstance();
-        var firstIndex = random.Next(destinationCount);
-        int secondIndex;
-        do
+
+        DestinationState first, second;
+        if (_weightingProvider is null)
         {
-            secondIndex = random.Next(destinationCount);
-        } while (firstIndex == secondIndex);
-        var first = availableDestinations[firstIndex];
-        var second = availableDestinations[secondIndex];
+            var random = _randomFactory.CreateRandomInstance();
+            var firstIndex = random.Next(destinationCount);
+            var secondIndex = random.Next(destinationCount - 1);
+            // account for the firstIndex by skipping it and moving beyond its index in the list
+            if (secondIndex >= firstIndex) { secondIndex++; }
+            first = availableDestinations[firstIndex];
+            second = availableDestinations[secondIndex];
+        }
+        else
+        {
+            first = WeightUtils.getRandomWeightedDestination(availableDestinations, _randomFactory);
+            second = WeightUtils.getRandomWeightedDestinationWithSkip(availableDestinations, first, _randomFactory);
+        }
         return (first.ConcurrentRequestCount <= second.ConcurrentRequestCount) ? first : second;
     }
 }
