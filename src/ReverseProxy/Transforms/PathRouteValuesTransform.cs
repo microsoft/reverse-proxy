44629d6ff3fdec9cb6bc58ce9e3a2cfc0f46664f
James Newton-King <james@newtonking.com>
2023-09-15 01:08:39 +0800

    Improve route string syntax usage (#2253)

--- src/ReverseProxy/Transforms/PathRouteValuesTransform.cs
+++ src/ReverseProxy/Transforms/PathRouteValuesTransform.cs
@@ -23,10 +23,7 @@ public class PathRouteValuesTransform : RequestTransform
     /// <param name="pattern">The pattern used to create the new request path.</param>
     /// <param name="binderFactory">The factory used to bind route parameters to the given path pattern.</param>
     public PathRouteValuesTransform(
-    #if NET7_0_OR_GREATER
-    [StringSyntax("Route")]
-    #endif
-    string pattern, TemplateBinderFactory binderFactory)
+        [StringSyntax("Route")] string pattern, TemplateBinderFactory binderFactory)
     {
         _ = pattern ?? throw new ArgumentNullException(nameof(pattern));
         _binderFactory = binderFactory ?? throw new ArgumentNullException(nameof(binderFactory));
---
+++ src/ReverseProxy/Utilities/StringSyntaxAttribute.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace System.Diagnostics.CodeAnalysis
+{
+#if !NET7_0_OR_GREATER
+    /// <summary>Specifies the syntax used in a string.</summary>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class StringSyntaxAttribute : Attribute
+    {
+        /// <summary>Initializes the <see cref="StringSyntaxAttribute"/> with the identifier of the syntax used.</summary>
+        /// <param name="syntax">The syntax identifier.</param>
+        public StringSyntaxAttribute(string syntax)
+        {
+            Syntax = syntax;
+            Arguments = Array.Empty<object?>();
+        }
+
+        /// <summary>Initializes the <see cref="StringSyntaxAttribute"/> with the identifier of the syntax used.</summary>
+        /// <param name="syntax">The syntax identifier.</param>
+        /// <param name="arguments">Optional arguments associated with the specific syntax employed.</param>
+        public StringSyntaxAttribute(string syntax, params object?[] arguments)
+        {
+            Syntax = syntax;
+            Arguments = arguments;
+        }
+
+        /// <summary>Gets the identifier of the syntax used.</summary>
+        public string Syntax { get; }
+
+        /// <summary>Optional arguments associated with the specific syntax employed.</summary>
+        public object?[] Arguments { get; }
+    }
+#endif
+}
