652a33a0f3914022ebfbabba6aeb032daaad9e4e
Daniel Steinmann <dsteinmann@techgroup.ch>
2023-10-01 17:18:20 +0200

    call to base

--- src/ReverseProxy/Transforms/RequestHeaderValueTransform.cs
+++ src/ReverseProxy/Transforms/RequestHeaderValueTransform.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Primitives;
 
 namespace Yarp.ReverseProxy.Transforms;
 
@@ -27,25 +26,7 @@ public class RequestHeaderValueTransform : RequestHeaderTransform
     /// <inheritdoc/>
     public override ValueTask ApplyAsync(RequestTransformContext context)
     {
-        if (context is null)
-        {
-            throw new ArgumentNullException(nameof(context));
-        }
-
-        if (Append)
-        {
-            var existingValues = TakeHeader(context, HeaderName);
-            var values = StringValues.Concat(existingValues, Value);
-            AddHeader(context, HeaderName, values);
-        }
-        else
-        {
-            // Set
-            RemoveHeader(context, HeaderName);
-            AddHeader(context, HeaderName, Value);
-        }
-
-        return default;
+        return base.ApplyAsync(context);
     }
 
     /// <inheritdoc/>
