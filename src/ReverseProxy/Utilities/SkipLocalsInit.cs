31d1f19502bc400dde8b7527227e045e904158ea
Miha Zupan <mihazupan.zupan1@gmail.com>
2023-10-03 01:10:41 +0200

    Use SkipLocalsInit on YARP module

---
+++ src/ReverseProxy/Utilities/SkipLocalsInit.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Used to indicate to the compiler that the .locals init flag should not be set in method headers.
+[module: System.Runtime.CompilerServices.SkipLocalsInit]
