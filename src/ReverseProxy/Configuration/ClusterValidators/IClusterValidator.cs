aea419ec7e4af24a5a58e98e0148a5a1c025d457
lbjn <lbjn@kamstrup.com>
2023-11-22 20:29:20 +0100

    Comments for public methods

--- src/ReverseProxy/Configuration/ClusterValidators/IClusterValidator.cs
+++ src/ReverseProxy/Configuration/ClusterValidators/IClusterValidator.cs
@@ -9,5 +9,12 @@ namespace Yarp.ReverseProxy.Configuration.ClusterValidators;
 /// </summary>
 public interface IClusterValidator
 {
+
+    /// <summary>
+    /// Perform validation on a cluster configuration by adding exceptions to the provided collection.
+    /// </summary>
+    /// <param name="cluster">Cluster configuration to validate</param>
+    /// <param name="errors">Collection of all validation exceptions</param>
+    /// <returns>A ValueTask representing the asynchronous validation operation.</returns>
     public ValueTask ValidateAsync(ClusterConfig cluster, IList<Exception> errors);
 }
