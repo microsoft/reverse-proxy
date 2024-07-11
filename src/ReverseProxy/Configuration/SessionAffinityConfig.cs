599b7131d2632e7d9b156bd899be242e69c592f3
Thomas Carlier <carlier.thomas@gmail.com>
2024-07-11 22:44:40 +1000

    Update src/ReverseProxy/Configuration/SessionAffinityConfig.cs
    Co-authored-by: Miha Zupan <mihazupan.zupan1@gmail.com>

--- src/ReverseProxy/Configuration/SessionAffinityConfig.cs
+++ src/ReverseProxy/Configuration/SessionAffinityConfig.cs
@@ -22,7 +22,7 @@ public sealed record SessionAffinityConfig
     public string? Policy { get; init; }
 
     /// <summary>
-    /// Strategy handling missing destination for a request by affinity.
+    /// Strategy for handling a missing destination for an affinitized request.
     /// </summary>
     public string? FailurePolicy { get; init; }
 
